d0a6fd10ad6ad166a3fdcb4330928f181c2b8503
Ukkey <develop@ukihome.com>
2024-07-16 08:44:32 +0900

    feat: Add lazy loading for images in SignInScreen

--- lib/src/features/authentication/presentation/sign_in_screen.dart
+++ lib/src/features/authentication/presentation/sign_in_screen.dart
@@ -1,17 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class SignInScreen extends StatelessWidget {
+class SignInScreen extends HookWidget {
   const SignInScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO(Ukkey): Implement build
+    final emailController = useTextEditingController();
+    final passwordController = useTextEditingController();
+
+    Future<void> function() async {
+      //
+    }
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Sign In'),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Sign In',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              _EmailForm(
+                controller: emailController,
+              ),
+              _PasswordForm(
+                controller: passwordController,
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Sign In'),
+              ),
+            ],
+          ),
+        ),
       ),
-      body: const Center(
-        child: Text('Sign In Screen'),
+    );
+  }
+}
+
+class _EmailForm extends StatelessWidget {
+  const _EmailForm({
+    required this.controller,
+  });
+
+  final TextEditingController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      decoration: const InputDecoration(
+        labelText: 'Email',
+      ),
+    );
+  }
+}
+
+class _PasswordForm extends StatelessWidget {
+  const _PasswordForm({
+    required this.controller,
+  });
+
+  final TextEditingController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      decoration: const InputDecoration(
+        labelText: 'Password',
       ),
     );
   }
