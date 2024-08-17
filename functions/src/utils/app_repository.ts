6b4fbd1b13f95dcbdc1025f038206895c0010a6b
Ukkey <develop@ukihome.com>
2024-08-18 00:11:31 +0900

    feat: Add AppRepository class for managing app data

---
+++ functions/src/utils/app_repository.ts
@@ -0,0 +1,49 @@
+import * as admin from 'firebase-admin'
+import { isNil } from 'lodash'
+import { AppModel } from './app_model'
+
+export abstract class AppRepository<T extends AppModel> {
+  constructor(private firestore: admin.firestore.Firestore) {}
+
+  //
+  // --- Paths ---
+  //
+
+  abstract collectionPath(): string
+
+  documentPath(id: string): string {
+    return `${this.collectionPath()}/${id}`
+  }
+
+  //
+  // --- References ---
+  //
+
+  collectionRef(): admin.firestore.CollectionReference<T> {
+    return this.firestore.collection(this.collectionPath()).withConverter(this.dataConverter)
+  }
+
+  documentRef(id?: string): admin.firestore.DocumentReference<T> {
+    return (
+      isNil(id)
+        ? this.firestore.collection(this.collectionPath()).doc()
+        : this.firestore.doc(this.documentPath(id))
+    ).withConverter(this.dataConverter)
+  }
+
+  //
+  // --- Data Converter ---
+  //
+
+  dataConverter: admin.firestore.FirestoreDataConverter<T> = {
+    fromFirestore: ds =>
+      ({
+        id: ds.id,
+        ...ds.data(),
+      }) as T,
+    toFirestore: data => {
+      delete data.id
+      return data
+    },
+  }
+}
