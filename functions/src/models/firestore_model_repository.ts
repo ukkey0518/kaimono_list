e9ccc25119bbb3c2fbf009607104147f2e55249c
Ukkey <develop@ukihome.com>
2024-08-21 08:50:33 +0900

    chore: Update dependencies and refactor user_profile and firestore_model_repository files

--- functions/src/models/firestore_model_repository.ts
+++ functions/src/models/firestore_model_repository.ts
@@ -1,7 +1,8 @@
 import * as admin from 'firebase-admin'
-import { isNil } from 'lodash'
+import { isNil, isUndefined } from 'lodash'
 import { FirestoreModel } from '../models/firestore_model'
 import { FirestoreRepository } from './firestore_repository'
+import assert = require('assert')
 
 export abstract class FirestoreModelRepository<
   T extends FirestoreModel,
@@ -46,5 +47,219 @@ export abstract class FirestoreModelRepository<
       delete data.id
       return data
     },
+  }
+
+  //
+  // --- GET ---
+  //
+
+  async get(id: string): Promise<T | undefined> {
+    const ds = await this.documentRef(id).get()
+    return ds.data()
+  }
+
+  async getWithTransaction(
+    transaction: admin.firestore.Transaction,
+    id: string
+  ): Promise<T | undefined> {
+    const ds = await transaction.get(this.documentRef(id))
+    return ds.data()
+  }
+
+  //
+  // --- EXISTS ---
+  //
+
+  async isExists(id: string): Promise<boolean> {
+    const ds = await this.documentRef(id).get()
+    return ds.exists
+  }
+
+  async isExistsWithTransaction(
+    transaction: admin.firestore.Transaction,
+    id: string
+  ): Promise<boolean> {
+    const ds = await transaction.get(this.documentRef(id))
+    return ds.exists
+  }
+
+  //
+  // --- ADD ---
+  //
+
+  async add(data: T): Promise<string> {
+    assert(isUndefined(data.id), 'id must be undefined')
+    const ref = await this.collectionRef().add(data)
+    return ref.id
+  }
+
+  async addWithTransaction(
+    transaction: admin.firestore.Transaction,
+    data: T
+  ): Promise<{
+    transaction: admin.firestore.Transaction
+    id: string
+  }> {
+    assert(isUndefined(data.id), 'id must be undefined')
+    const ref = this.documentRef()
+    transaction.set(ref, data)
+    return {
+      transaction,
+      id: ref.id,
+    }
+  }
+
+  async addWithBatch(
+    batch: admin.firestore.WriteBatch,
+    data: T
+  ): Promise<{
+    batch: admin.firestore.WriteBatch
+    id: string
+  }> {
+    assert(isUndefined(data.id), 'id must be undefined')
+    const ref = this.documentRef()
+    batch.set(ref, data)
+    return {
+      batch,
+      id: ref.id,
+    }
+  }
+
+  //
+  // --- CREATE ---
+  //
+
+  async create(data: T): Promise<void> {
+    assert(data.id, 'id is required')
+    await this.documentRef(data.id).create(data)
+  }
+
+  async createWithTransaction(
+    transaction: admin.firestore.Transaction,
+    data: T
+  ): Promise<admin.firestore.Transaction> {
+    assert(data.id, 'id is required')
+    transaction.create(this.documentRef(data.id), data)
+    return transaction
+  }
+
+  async createWithBatch(
+    batch: admin.firestore.WriteBatch,
+    data: T
+  ): Promise<admin.firestore.WriteBatch> {
+    assert(data.id, 'id is required')
+    batch.create(this.documentRef(data.id), data)
+    return batch
+  }
+
+  //
+  // --- SET ---
+  //
+
+  async set(data: T): Promise<void>
+  async set(data: T, options: admin.firestore.SetOptions): Promise<void>
+  async set(data: T, options?: admin.firestore.SetOptions): Promise<void> {
+    assert(data.id, 'id is required')
+    if (options) {
+      await this.documentRef(data.id).set(data, options)
+    } else {
+      await this.documentRef(data.id).set(data)
+    }
+  }
+
+  async setWithTransaction(
+    transaction: admin.firestore.Transaction,
+    data: T
+  ): Promise<admin.firestore.Transaction>
+  async setWithTransaction(
+    transaction: admin.firestore.Transaction,
+    data: T,
+    options: admin.firestore.SetOptions
+  ): Promise<admin.firestore.Transaction>
+  async setWithTransaction(
+    transaction: admin.firestore.Transaction,
+    data: T,
+    options?: admin.firestore.SetOptions
+  ): Promise<admin.firestore.Transaction> {
+    assert(data.id, 'id is required')
+    if (options) {
+      transaction.set(this.documentRef(data.id), data, options)
+    } else {
+      transaction.set(this.documentRef(data.id), data)
+    }
+    return transaction
+  }
+
+  async setWithBatch(
+    batch: admin.firestore.WriteBatch,
+    data: T
+  ): Promise<admin.firestore.WriteBatch>
+  async setWithBatch(
+    batch: admin.firestore.WriteBatch,
+    data: T,
+    options: admin.firestore.SetOptions
+  ): Promise<admin.firestore.WriteBatch>
+  async setWithBatch(
+    batch: admin.firestore.WriteBatch,
+    data: T,
+    options?: admin.firestore.SetOptions
+  ): Promise<admin.firestore.WriteBatch> {
+    assert(data.id, 'id is required')
+    if (options) {
+      batch.set(this.documentRef(data.id), data, options)
+    } else {
+      batch.set(this.documentRef(data.id), data)
+    }
+    return batch
+  }
+  
+  //
+  // --- UPDATE ---
+  //
+
+  async update(id: string, data: object): Promise<void> {
+    await this.documentRef(id).update(data)
+  }
+
+  async updateWithTransaction(
+    transaction: admin.firestore.Transaction,
+    id: string,
+    data: object
+  ): Promise<admin.firestore.Transaction> {
+    transaction.update(this.documentRef(id), data)
+    return transaction
+  }
+
+  async updateWithBatch(
+    batch: admin.firestore.WriteBatch,
+    id: string,
+    data: object
+  ): Promise<admin.firestore.WriteBatch> {
+    batch.update(this.documentRef(id), data)
+    return batch
+  }
+
+  //
+  // --- DELETE ---
+  //
+
+  async delete(id: string): Promise<void> {
+    await this.documentRef(id).delete()
+  }
+
+  async deleteWithTransaction(
+    transaction: admin.firestore.Transaction,
+    id: string
+  ): Promise<admin.firestore.Transaction> {
+    transaction.delete(this.documentRef(id))
+    return transaction
+  }
+
+  async deleteWithBatch(
+    batch: admin.firestore.WriteBatch,
+    id: string
+  ): Promise<admin.firestore.WriteBatch> {
+    batch.delete(this.documentRef(id))
+    return batch
   }
 }
--- functions/src/models/user_profile/user_profile_repository.ts
+++ functions/src/models/user_profile/user_profile_repository.ts
@@ -5,18 +5,4 @@ export class UserProfileRepository extends FirestoreModelRepository<UserProfile>
   collectionPath(): string {
     return '/user_profiles'
   }
-
-  async get(id: string): Promise<UserProfile | undefined> {
-    const ds = await this.documentRef(id).get()
-    return ds.data()
-  }
-
-  async set(data: UserProfile, { merge = true }): Promise<void> {
-    await this.documentRef(data.id).set(data, { merge })
-  }
-
-  async isExists(id: string): Promise<boolean> {
-    const ds = await this.documentRef(id).get()
-    return ds.exists
-  }
 }
