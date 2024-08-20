d22957353ebc627397f982c8045d00726f8934c8
Ukkey <develop@ukihome.com>
2024-08-21 07:49:59 +0900

    chore: Refactor user_profile.ts and user_profile_repository.ts file structure

--- functions/src/models/user_profile/user_profile_repository.ts
+++ functions/src/models/user_profile/user_profile_repository.ts
@@ -1,18 +1,18 @@
-import { FirestoreModelRepository } from './firestore_model_repository'
-import { UserProfile } from '../models/user_profile'
+import { FirestoreModelRepository } from '../firestore_model_repository'
+import { UserProfile } from './user_profile'
 
 export class UserProfileRepository extends FirestoreModelRepository<UserProfile> {
   collectionPath(): string {
-    return 'user_profiles'
-  }
-
-  async set(data: UserProfile, { merge = true }): Promise<void> {
-    await this.documentRef(data.id).set(data, { merge })
+    return '/user_profiles'
   }
 
   async get(id: string): Promise<UserProfile | undefined> {
     const ds = await this.documentRef(id).get()
     return ds.data()
+  }
+
+  async set(data: UserProfile, { merge = true }): Promise<void> {
+    await this.documentRef(data.id).set(data, { merge })
   }
 
   async isExists(id: string): Promise<boolean> {
