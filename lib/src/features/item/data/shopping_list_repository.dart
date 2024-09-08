013686d8a7e1c4edd6a5297aba0e726a7acf7a78
Ukkey <develop@ukihome.com>
2024-09-08 19:12:26 +0900

    refactor: Add ShoppingListRepository and its generated code
    This commit adds the `ShoppingListRepository` class and its generated code. The `ShoppingListRepository` is responsible for handling CRUD operations related to shopping lists in the app. It includes methods for creating and updating shopping lists, as well as retrieving references to shopping lists from Firestore.
    
    The generated code includes the `shoppingListRepositoryProvider` and `ShoppingListRepositoryRef` definitions, which are used for dependency injection and managing the lifecycle of the repository.
    
    This commit does not introduce any functional changes to the app, but lays the foundation for future development related to shopping lists.

---
+++ lib/src/features/item/data/shopping_list_repository.dart
@@ -0,0 +1,53 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:kaimono_list/src/features/item/domain/shopping_list.dart';
+import 'package:kaimono_list/src/utils/extensions/firestore_extensions.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'shopping_list_repository.g.dart';
+
+class ShoppingListRepository {
+  const ShoppingListRepository(this._firestore);
+
+  final FirebaseFirestore _firestore;
+
+  // ------------------------------------------------------------------------ //
+
+  static String shoppingListsPath() => 'shopping_lists';
+
+  static String shoppingListPath(String id) => '${shoppingListsPath()}/$id';
+
+  CollectionReference<ShoppingList> shoppingListsRef() {
+    return _firestore.collection(shoppingListsPath()).withConverter(
+          fromFirestore: (ds, _) => ShoppingList.fromJson(ds.toModelData()!),
+          toFirestore: (shoppingList, _) => shoppingList.toJson(),
+        );
+  }
+
+  DocumentReference<ShoppingList> shoppingListRef(String id) {
+    return _firestore.doc(shoppingListPath(id)).withConverter(
+          fromFirestore: (ds, _) => ShoppingList.fromJson(ds.toModelData()!),
+          toFirestore: (shoppingList, _) => shoppingList.toJson(),
+        );
+  }
+
+  // ------------------------------------------------------------------------ //
+
+  Future<String> createShoppingList(ShoppingList shoppingList) async {
+    final ref = await shoppingListsRef().add(shoppingList);
+    return ref.id;
+  }
+
+  Future<void> updateShoppingList(ShoppingList shoppingList) async {
+    await shoppingListRef(shoppingList.id!).set(
+      shoppingList,
+      SetOptions(merge: true),
+    );
+  }
+}
+
+@Riverpod(
+  dependencies: [],
+)
+ShoppingListRepository shoppingListRepository(ShoppingListRepositoryRef ref) {
+  return ShoppingListRepository(FirebaseFirestore.instance);
+}
---
+++ lib/src/features/item/data/shopping_list_repository.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'shopping_list_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$shoppingListRepositoryHash() =>
+    r'6b2b6d99fe539e6ecd65230ce324e4f967df7cbf';
+
+/// See also [shoppingListRepository].
+@ProviderFor(shoppingListRepository)
+final shoppingListRepositoryProvider =
+    AutoDisposeProvider<ShoppingListRepository>.internal(
+  shoppingListRepository,
+  name: r'shoppingListRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$shoppingListRepositoryHash,
+  dependencies: const <ProviderOrFamily>[],
+  allTransitiveDependencies: const <ProviderOrFamily>{},
+);
+
+typedef ShoppingListRepositoryRef
+    = AutoDisposeProviderRef<ShoppingListRepository>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
