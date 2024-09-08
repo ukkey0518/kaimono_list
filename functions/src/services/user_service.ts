cc413e287b7898c2244065d38d4afa3ce639e5d9
Ukkey <develop@ukihome.com>
2024-09-08 16:52:48 +0900

    feat: Add generateDefaultUserName and createNewUserProfile methods to UserService

--- functions/src/services/user_service.ts
+++ functions/src/services/user_service.ts
@@ -1,15 +1,44 @@
 import { UserProfileRepository } from '../repositories/user_profile_repository'
+import { generateRandomUsername } from '../utils/user_name_generator'
+import * as logger from 'firebase-functions/logger'
 
 export class UserService {
   constructor(private userProfileRepository: UserProfileRepository) {}
 
-  // Create an empty user profile if it does not exist.
-  // This is called when a new user is created.
-  // If the user profile already exists, this function does nothing.
-  async createEmptyUserProfile(userId: string): Promise<void> {
-    if (await this.userProfileRepository.isExists(userId)) {
-      return
+  /**
+   * Generates a random username and checks if it already exists in the user profile repository.
+   * If it does, a new username is generated until a unique one is found.
+   *
+   * @returns {Promise<string>} - A Promise resolving to a unique generated username.
+   */
+  async generateDefaultUserName(): Promise<string> {
+    let userName = generateRandomUsername()
+    logger.debug(`Generated default username: ${userName}`)
+    while (await this.userProfileRepository.isUserNameMatched(userName)) {
+      logger.debug(`Username ${userName} already exists. Regenerating...`)
+      userName = generateRandomUsername()
     }
-    await this.userProfileRepository.create(userId, {})
+    logger.debug(`Unique username generated: ${userName}`)
+    return userName
+  }
+
+  /**
+   * Creates a new user profile with a default username if the profile does not already exist for the given user ID.
+   *
+   * @param {string} userId - The unique identifier of the user for whom the profile is being created.
+   * @returns {Promise<boolean>} - A Promise that resolves to `true` if a new profile is successfully created,
+   * and `false` if a profile already exists for the given `userId`.
+   */
+  async createNewUserProfile(userId: string): Promise<boolean> {
+    if (await this.userProfileRepository.isExists(userId)) {
+      logger.debug(`User profile already exists for user ID: ${userId}`)
+      return false
+    }
+    const userName = await this.generateDefaultUserName()
+    await this.userProfileRepository.create(userId, {
+      name: userName,
+    })
+    logger.debug(`New user profile created for user ID: ${userId}`)
+    return true
   }
 }
