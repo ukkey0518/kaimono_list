6b9692935cf728996f747c108967b38e948c1573
Ukkey <develop@ukihome.com>
2024-09-08 16:52:40 +0900

    refactor: Add isUserNameMatched method to UserProfileRepository

--- functions/src/repositories/user_profile_repository.ts
+++ functions/src/repositories/user_profile_repository.ts
@@ -5,4 +5,16 @@ export class UserProfileRepository extends FirestoreModelRepository<UserProfileD
   collectionPath(): string {
     return '/user_profiles'
   }
+
+  /**
+   * Checks if the specified username exists in the collection
+   * and returns a boolean indicating whether it matches.
+   *
+   * @param {string} userName - The username to check.
+   * @returns {Promise<boolean>} - A Promise resolving to a boolean indicating the match result.
+   */
+  async isUserNameMatched(userName: string): Promise<boolean> {
+    const ds = await this.collectionRef().where('name', '==', userName).get()
+    return !ds.empty
+  }
 }
