65a2361583e7ea5cb205a3f4f8b9c52af92522bd
Ukkey <develop@ukihome.com>
2024-09-11 10:09:19 +0900

    feat: Add ModelValidationException and ValidatableModel interfaces

---
+++ lib/src/exceptions/model_validation_exception.dart
@@ -0,0 +1,12 @@
+class ModelValidationException implements Exception {
+  ModelValidationException(
+    this.message,
+    this.data,
+  );
+
+  final String message;
+  final dynamic data;
+
+  @override
+  String toString() => 'ModelValidationException: $message {data: $data}';
+}
