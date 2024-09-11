0f3f56fe466603205edf13fe5868d486e5da8df2
Ukkey <develop@ukihome.com>
2024-09-11 21:34:18 +0900

    feat: Add listByOwnerUserId to ShoppingListRepository

--- functions/src/repositories/shopping_list_repository.ts
+++ functions/src/repositories/shopping_list_repository.ts
@@ -1,4 +1,4 @@
-import { ShoppingList, ShoppingListData } from '../models/shoppng_list'
+import { ShoppingList, ShoppingListData } from '../models/shopping_list'
 import { FirestoreModelRepository } from './firestore_model_repository'
 
 export class ShoppingListRepository extends FirestoreModelRepository<
@@ -7,5 +7,16 @@ export class ShoppingListRepository extends FirestoreModelRepository<
 > {
   collectionPath(): string {
     return '/shopping_lists'
+  }
+
+  async listByOwnerUserId(ownerUserId: string): Promise<ShoppingList[]> {
+    const qs = await this.firestore
+      .collection(this.collectionPath())
+      .where('ownerUserId', '==', ownerUserId)
+      .get()
+    return qs.docs.map(doc => ({
+      id: doc.id,
+      ...(doc.data() as ShoppingListData),
+    }))
   }
 }
