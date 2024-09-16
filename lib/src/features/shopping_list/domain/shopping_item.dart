5512eb5a71b5753a0fdd7e9465ae21ba96c7115b
Ukkey <develop@ukihome.com>
2024-09-16 12:13:18 +0900

    feat: Add ShoppingItem model

---
+++ lib/src/features/shopping_list/domain/shopping_item.dart
@@ -0,0 +1,41 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:kaimono_list/src/utils/annotations/firestore_annotations.dart';
+import 'package:kaimono_list/src/utils/types/validatable_model.dart';
+
+part 'shopping_item.freezed.dart';
+part 'shopping_item.g.dart';
+
+@freezed
+class ShoppingItem with _$ShoppingItem implements ValidatableModel {
+  @firestoreModel
+  const factory ShoppingItem({
+    required String name,
+    @Default(false) bool isPurchased,
+    @firestoreId String? id,
+    @firestoreCreatedAt DateTime? createdAt,
+    @firestoreUpdatedAt DateTime? updatedAt,
+  }) = _ShoppingItem;
+
+  factory ShoppingItem.fromJson(Map<String, dynamic> json) =>
+      _$ShoppingItemFromJson(json);
+
+  const ShoppingItem._();
+
+  static int get maxNameLength => 20;
+
+  @override
+  String? validateForCreate() {
+    final name = this.name;
+
+    if (name.isEmpty) {
+      return '`name` is required.';
+    }
+    if (name.length > maxNameLength) {
+      return '`name` must be at most $maxNameLength characters.';
+    }
+    if (isPurchased) {
+      return '`isPurchased` must be false.';
+    }
+    return null;
+  }
+}
