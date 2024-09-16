7a8d5e64ebb0cfcc0c016551ae8751bb12af66f1
Ukkey <develop@ukihome.com>
2024-09-17 07:53:31 +0900

    feat: Add ShoppingItemCleanFab component

---
+++ lib/src/features/shopping_list/presentation/shopping_list/components/shopping_item_clean_fab.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:kaimono_list/src/constants/sizes.dart';
+import 'package:kaimono_list/src/utils/extensions/string_extensions.dart';
+
+class ShoppingItemCleanFab extends StatelessWidget {
+  const ShoppingItemCleanFab({
+    required this.onPressed,
+    super.key,
+  });
+
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO(Ukkey): Explain the behavior of this button in some way
+    return Tooltip(
+      message: '購入済のアイテムを削除'.hardcoded,
+      // * Adjust verticalOffset to show Tooltip above the Widget
+      verticalOffset: -Sizes.p64,
+      child: FloatingActionButton.small(
+        heroTag: 'cleanShoppingItems',
+        // TODO(Ukkey): Add elevation to the theme
+        elevation: 4,
+        onPressed: onPressed,
+        child: const Icon(Icons.cleaning_services_rounded),
+      ),
+    );
+  }
+}
