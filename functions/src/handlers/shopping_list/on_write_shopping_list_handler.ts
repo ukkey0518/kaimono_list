9461326d2c92385f8cd6385b3184b6216d8d550e
Ukkey <develop@ukihome.com>
2024-09-20 09:16:36 +0900

    doc: Add comment

--- functions/src/handlers/shopping_list/on_write_shopping_list_handler.ts
+++ functions/src/handlers/shopping_list/on_write_shopping_list_handler.ts
@@ -1,5 +1,15 @@
 import { AppState } from '../../app_state'
 
+/**
+ * Handles the deletion of a shopping list.
+ *
+ * This function is triggered when a shopping list document is deleted.
+ * It ensures that the shopping list is removed from all users' data.
+ *
+ * @param appState - The current application state, which includes services and other app-wide data.
+ * @param ds - The document snapshot of the shopping list that was deleted. If undefined, the function logs an error and returns.
+ * @returns A promise that resolves when the shopping list has been successfully deleted from all users.
+ */
 export async function onDeleteShoppingListHandler(
   appState: AppState,
   ds: FirebaseFirestore.QueryDocumentSnapshot | undefined
