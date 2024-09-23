e7bc4fb6d9bc1abde2d02ff13c937299a88966d0
Ukkey <develop@ukihome.com>
2024-09-23 17:56:48 +0900

    refactor: Update user_id parameter in auth user handlers to use functionsV1

--- functions/src/handlers/auth/triggers/on_delete_auth_user_handler.ts
+++ functions/src/handlers/auth/triggers/on_delete_auth_user_handler.ts
@@ -1,9 +1,9 @@
-import * as functions from 'firebase-functions/v1'
+import * as functionsV1 from 'firebase-functions/v1'
 import { AppState } from '../../../app_state'
 
 export async function onDeleteAuthUserHandler(
   appState: AppState,
-  user: functions.auth.UserRecord
+  user: functionsV1.auth.UserRecord
 ): Promise<void> {
   await appState.userService.setUserStatusToDeleted(user.uid)
 }
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,5 @@
 import * as admin from 'firebase-admin'
-import * as functions from 'firebase-functions/v1'
+import * as functionsV1 from 'firebase-functions/v1'
 import * as functionsV2 from 'firebase-functions/v2'
 import { AppState } from './app_state'
 import { onCreateAuthUserHandler } from './handlers/auth/triggers/on_create_auth_user_handler'
@@ -16,12 +16,12 @@ const appState = new AppState(admin.initializeApp())
 // Functions
 //
 
-const onCreateAuthUser = functions
+const onCreateAuthUser = functionsV1
   .region('asia-northeast1')
   .auth.user()
   .onCreate(async user => await onCreateAuthUserHandler(appState, user))
 
-const onDeleteAuthUser = functions
+const onDeleteAuthUser = functionsV1
   .region('asia-northeast1')
   .auth.user()
   .onDelete(async user => await onDeleteAuthUserHandler(appState, user))
