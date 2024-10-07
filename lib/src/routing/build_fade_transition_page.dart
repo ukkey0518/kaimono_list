95c2b9d2dfc42866e9e3a8a1a6f0b2350ade7549
Ukkey <develop@ukihome.com>
2024-10-08 08:14:02 +0900

    feat: Add buildFadeTransitionPage function

---
+++ lib/src/routing/build_fade_transition_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+Page<T> buildFadeTransitionPage<T>({
+  required BuildContext context,
+  required GoRouterState state,
+  required Widget child,
+  bool fullscreenDialog = false,
+  Color? barrierColor,
+  bool barrierDismissible = true,
+  Duration transitionDuration = const Duration(milliseconds: 300),
+  Duration reverseTransitionDuration = const Duration(milliseconds: 300),
+}) {
+  return CustomTransitionPage<T>(
+    fullscreenDialog: fullscreenDialog,
+    barrierColor: barrierColor,
+    barrierDismissible: barrierDismissible,
+    transitionDuration: transitionDuration,
+    reverseTransitionDuration: reverseTransitionDuration,
+    transitionsBuilder: (context, animation, _, child) {
+      return FadeTransition(
+        opacity: animation,
+        child: child,
+      );
+    },
+    child: child,
+  );
+}
