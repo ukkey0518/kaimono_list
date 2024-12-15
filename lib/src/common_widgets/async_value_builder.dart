a146233b6bf53895b8c6b4a4c90079cf50d5cb0c
Ukkey <develop@ukihome.com>
2024-12-15 10:05:16 +0900

    feat: Add AsyncValueBuilder widget for handling async states with retry functionality

---
+++ lib/src/common_widgets/async_value_builder.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:kaimono_list/src/common_widgets/retry_button.dart';
+
+class AsyncValueBuilder<T> extends StatelessWidget {
+  const AsyncValueBuilder({
+    required this.asyncValue,
+    required this.builder,
+    required this.onRetry,
+    super.key,
+  });
+
+  final AsyncValue<T> asyncValue;
+  final Widget Function(BuildContext context, T value) builder;
+  final VoidCallback onRetry;
+
+  @override
+  Widget build(BuildContext context) {
+    return switch (asyncValue) {
+      AsyncLoading() => const Center(child: CircularProgressIndicator()),
+      AsyncError() => RetryButton(onPressed: onRetry),
+      AsyncData(:final value) => builder(context, value),
+      _ => const SizedBox(),
+    };
+  }
+}
